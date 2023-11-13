19147c3deead08d31f394f657599001c1ec9a3c1
heying <131231076+hydoris@users.noreply.github.com>
2023-11-13 12:47:09 +0900

    Delete コーディング directory

--- コーディング/js/work.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load resize', function() {
-    var windowWidth = window.innerWidth;
-    var elements = $('#fixed-area');
-    if (windowWidth >= 768) {
-    Stickyfill.add(elements);
-    }else{
-    Stickyfill.remove(elements);
-    } 
-    });
